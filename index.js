c73958b7f33c7c29277d7ab5b7141e97e46ab5f2
Baxtiyar Mammadyarov <74107721+BaxtiyarMammadyarov@users.noreply.github.com>
2023-02-15 21:57:59 +0400

    data

--- index.js
+++ index.js
@@ -1,113 +1,67 @@
-const btn_azn_in = document.querySelector("#btn-azn-in");
-const btn_eur_in = document.querySelector("#btn-eur-in");
-const btn_usd_in = document.querySelector("#btn-usd-in");
-const btn_gbp_in = document.querySelector("#btn-gbp-in");
-const btn_azn_out = document.querySelector("#btn-azn-out");
-const btn_eur_out = document.querySelector("#btn-eur-out");
-const btn_usd_out = document.querySelector("#btn-usd-out");
-const btn_gbp_out = document.querySelector("#btn-gbp-out");
 
 let fromValyuta = '';
 let toValyuta = '';
-async function fetchMetod(btn, fromValyuta, toValyuta, input, out) {
-  
+
+async function fetchMetod(btn, fromValyuta, toValyuta, input, out, input_req_sm, input_res_sm) {
+    console.log("fromValyuta --  ", fromValyuta)
+    console.log("toValyuta --  ", toValyuta)
     const res = await fetch(`https://api.exchangerate.host/latest?base=${fromValyuta}&symbols=${toValyuta}`);
     const data = await res.json();
-    const kurs = parseFloat(data.rates[toValyuta]);
-    const amount = parseFloat(input.value)
-    out.value = amount * kurs;
+    let kurs = parseFloat(data.rates[toValyuta]);
+    let kursR = 1 / kurs
+    const amount = parseFloat(input.value.length === 0 ? "1" : input.value)
+    let result = amount * kurs;
+    out.value = result.toFixed(2);
     console.log(data.rates[toValyuta]);
+    input_req_sm.innerHTML = "1 " + fromValyuta + " = " + kurs.toFixed(2) + " " + toValyuta;
+    input_res_sm.innerHTML = "1 " + toValyuta + " = " + kursR.toFixed(2) + " " + fromValyuta;
+    console.log("fromValyuta  ", fromValyuta)
+    console.log("toValyuta  ", toValyuta)
+}
+
+function disableEnabeleBtn(btn, value, active) {
+    btn.forEach(bt => {
+        bt.disabled = value
+        if (active === 1) {
+            bt.classList.remove('active')
+        }
+    })
 
 }
 
-function disableEnabeleBtn(btn, value) {
-    btn.forEach(bt => bt.disabled = value)
 
-}
-
-
-function setValyuta(btn, input, out) {
+function setValyuta(btn, input, out, input_req_sm, input_res_sm) {
     if (fromValyuta.length === 0) {
-        console.log("fromValyuta ", fromValyuta)
-    
         fromValyuta = btn.value;
+
     } else {
         toValyuta = btn.value;
-        console.log("toValyuta ", toValyuta)
-        fetchMetod(btn, fromValyuta, toValyuta, input, out)
+        fetchMetod(btn, fromValyuta, toValyuta, input, out, input_req_sm, input_res_sm)
+        fromValyuta = btn.value;
 
     }
 }
 
-function onClickBtn(event){
-   const btn = event.target
-   const input = document.querySelector("#input-req");
-   const out = document.querySelector("#input-res");
-const btn_out = document.querySelectorAll(".btn-out");
-const btn_in = document.querySelectorAll(".btn-in");
-  if(event.target.classList.contains('btn-out')){
-     setValyuta(btn, input, out);
-    disableEnabeleBtn(btn_in, false);
-    disableEnabeleBtn(btn_out, true);
-  }
-  else if(event.target.classList.contains('btn-in')){
-    setValyuta(btn, out, input);
-    disableEnabeleBtn(btn_in, true);
-    disableEnabeleBtn(btn_out, false);
-  }
+function onClickBtn(event) {
+    const btn = event.target
+    const input = document.querySelector("#input-req");
+    const out = document.querySelector("#input-res");
+    const btn_out = document.querySelectorAll(".btn-out");
+    const btn_in = document.querySelectorAll(".btn-in");
+    let input_req_sm = document.querySelector("#input-req-sm");
+    let input_res_sm = document.querySelector("#input-res-sm");
+    if (event.target.classList.contains('btn-out')) {
+        btn.classList.add("active")
+        setValyuta(btn, input, out, input_req_sm, input_res_sm);
+        disableEnabeleBtn(btn_in, false, 0);
+        disableEnabeleBtn(btn_out, true, 1);
+    }
+    else if (event.target.classList.contains('btn-in')) {
+
+        setValyuta(btn, out, input, input_res_sm, input_req_sm);
+        disableEnabeleBtn(btn_in, true, 1);
+        disableEnabeleBtn(btn_out, false, 0);
+    }
 
 }
 
-// btn_azn_in.addEventListener('click', (e) => {
-
-//     setValyuta(btn_azn_in, out, input);
-//     disableEnabeleBtn(btn_in, true);
-//     disableEnabeleBtn(btn_out, false);
-
-// })
-// btn_eur_in.addEventListener('click', (e) => {
-
-//     setValyuta(btn_azn_in, out, input);
-//     disableEnabeleBtn(btn_in, true);
-//     disableEnabeleBtn(btn_out, false);
-
-// })
-// btn_usd_in.addEventListener('click', (e) => {
-
-//     setValyuta(btn_azn_in, out, input);
-//     disableEnabeleBtn(btn_in, true);
-//     disableEnabeleBtn(btn_out, false);
-
-// })
-// btn_gbp_in.addEventListener('click', (e) => {
-
-//     setValyuta(btn_azn_in, out, input);
-//     disableEnabeleBtn(btn_in, true);
-//     disableEnabeleBtn(btn_out, false);
-
-// })
-// btn_azn_out.addEventListener('click', (e) => {
-
-//     setValyuta(btn_azn_in, input, out);
-//     disableEnabeleBtn(btn_in, false);
-//     disableEnabeleBtn(btn_out, true);
-
-// })
-// btn_eur_out.addEventListener('click', (e) => {
-//     setValyuta(btn_azn_in, input, out);
-//     disableEnabeleBtn(btn_in, false);
-//     disableEnabeleBtn(btn_out, true);
-
-// })
-// btn_usd_out.addEventListener('click', (e) => {
-//     setValyuta(btn_azn_in, input, out);
-//     disableEnabeleBtn(btn_in, false);
-//     disableEnabeleBtn(btn_out, true);
-
-// })
-// btn_gbp_out.addEventListener('click', (e) => {
-//     setValyuta(btn_azn_in, input, out);
-//     disableEnabeleBtn(btn_in, false);
-//     disableEnabeleBtn(btn_out, true);
-
-// })
