f6a910546365c4a1ddb9ac677de47c701d2f660b
Baxtiyar Mammadyarov <74107721+BaxtiyarMammadyarov@users.noreply.github.com>
2023-02-17 01:14:58 +0400

    responsive

--- index.js
+++ index.js
@@ -1,67 +1,58 @@
 
-let fromValyuta = '';
-let toValyuta = '';
-
-async function fetchMetod(btn, fromValyuta, toValyuta, input, out, input_req_sm, input_res_sm) {
-    console.log("fromValyuta --  ", fromValyuta)
-    console.log("toValyuta --  ", toValyuta)
+let fromValyuta = 'AZN';
+let toValyuta = 'USD';
+let menu = true;
+const input = document.querySelector("#input-req");
+const out = document.querySelector("#input-res");
+const input_title = document.querySelector("#input-req");
+const out_title = document.querySelector("#input-res");
+const login = document.querySelector(".login");
+const title_login = document.querySelector(".title-login");
+const toggle = document.querySelector(".toggle");
+async function fetchMetod(btn, fromValyuta, toValyuta) {
+  
     const res = await fetch(`https://api.exchangerate.host/latest?base=${fromValyuta}&symbols=${toValyuta}`);
     const data = await res.json();
     let kurs = parseFloat(data.rates[toValyuta]);
     let kursR = 1 / kurs
-    const amount = parseFloat(input.value.length === 0 ? "1" : input.value)
+    const amount = parseFloat(input.value)
     let result = amount * kurs;
     out.value = result.toFixed(2);
     console.log(data.rates[toValyuta]);
-    input_req_sm.innerHTML = "1 " + fromValyuta + " = " + kurs.toFixed(2) + " " + toValyuta;
-    input_res_sm.innerHTML = "1 " + toValyuta + " = " + kursR.toFixed(2) + " " + fromValyuta;
-    console.log("fromValyuta  ", fromValyuta)
-    console.log("toValyuta  ", toValyuta)
-}
-
-function disableEnabeleBtn(btn, value, active) {
-    btn.forEach(bt => {
-        bt.disabled = value
-        if (active === 1) {
-            bt.classList.remove('active')
-        }
-    })
-
-}
-
-
-function setValyuta(btn, input, out, input_req_sm, input_res_sm) {
-    if (fromValyuta.length === 0) {
-        fromValyuta = btn.value;
-
-    } else {
-        toValyuta = btn.value;
-        fetchMetod(btn, fromValyuta, toValyuta, input, out, input_req_sm, input_res_sm)
-        fromValyuta = btn.value;
-
-    }
+    input_title.innerHTML = "1 " + fromValyuta + " = " + kurs.toFixed(2) + " " + toValyuta;
+    out_title.innerHTML = "1 " + toValyuta + " = " + kursR.toFixed(2) + " " + fromValyuta;
 }
 
 function onClickBtn(event) {
     const btn = event.target
-    const input = document.querySelector("#input-req");
-    const out = document.querySelector("#input-res");
-    const btn_out = document.querySelectorAll(".btn-out");
-    const btn_in = document.querySelectorAll(".btn-in");
-    let input_req_sm = document.querySelector("#input-req-sm");
-    let input_res_sm = document.querySelector("#input-res-sm");
     if (event.target.classList.contains('btn-out')) {
-        btn.classList.add("active")
-        setValyuta(btn, input, out, input_req_sm, input_res_sm);
-        disableEnabeleBtn(btn_in, false, 0);
-        disableEnabeleBtn(btn_out, true, 1);
+        btn.classList.add("active");
+        toValyuta = event.target.value;
+        document.querySelectorAll(".btn-out").forEach((item) => (item.value !== btn.value) ? item.classList.remove("active") : "") ;
+        fetchMetod(btn, fromValyuta, toValyuta)
     }
     else if (event.target.classList.contains('btn-in')) {
-
-        setValyuta(btn, out, input, input_res_sm, input_req_sm);
-        disableEnabeleBtn(btn_in, true, 1);
-        disableEnabeleBtn(btn_out, false, 0);
+        btn.classList.add("active");
+        fromValyuta = event.target.value;
+        document.querySelectorAll(".btn-in").forEach((item) => (item.value !== btn.value) ? item.classList.remove("active") : "");
+        fetchMetod(btn, fromValyuta, toValyuta)
     }
-
 }
+
+toggle.addEventListener('click',(e) =>{
+    e.preventDefault()
+    if(menu && window.innerWidth < 880){
+        title_login.classList.add("title-login-active")
+        menu = false;
+    }
+    else if (window.innerWidth > 880){
+        title_login.classList.remove("title-login-active");
+    }
+    else  {
+        title_login.classList.remove("title-login-active");
+        menu = true;
+    }
+    
+})
+
 
